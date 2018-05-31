719943d2ebdc66c3b7de482d39605de1f95cefb9
Yu Chen <yu.chen@live.ie>
2018-05-31 13:48:18 +0800

    aot

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule, ModuleWithProviders, InjectionToken } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JSSDK } from './service'
 
 export * from './service';
+
+export function JSSDKFactory(appId: string, debug: boolean) {
+  return new JSSDK(appId, debug);
+}
+
+export const APPID = new InjectionToken<string>('wechat_mp_app_id');
+export const DEBUG = new InjectionToken<string>('wechat_mp_debug');
+
 
 @NgModule({
   imports: [
@@ -15,10 +23,11 @@ export class JSSDKModule {
   static forRoot(appId: string, debug: boolean = false): ModuleWithProviders {
     return {
       ngModule: JSSDKModule,
-      providers: [{
-        provide: JSSDK,
-        useValue: new JSSDK(appId, debug),
-      }]
+      providers: [
+        { provide: APPID, useValue: appId },
+        { provide: DEBUG, useValue: debug },
+        { provide: JSSDK, useFactory: JSSDKFactory, deps: [APPID, DEBUG] },
+      ]
     };
   }
 }
--- src/service.ts
+++ src/service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Wechat } from '@yct/jssdk';
+import { APPID, DEBUG } from './index';
 
 @Injectable()
 export class JSSDK extends Wechat {
-  
+  constructor(
+    @Inject(APPID) public appId: string,
+    @Inject(APPID) public debug: boolean,
+  ) {
+    super(appId, debug);
+  }
 }
